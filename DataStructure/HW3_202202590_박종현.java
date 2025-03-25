ae8520a63db5260f1e85efbbc40cc9a1baad4969
JongHyeon Park <jonghp1128@naver.com>
2025-03-25 16:59:49 +0900

    테스트

--- DataStructure/HW3_202202590_박종현.java
+++ DataStructure/HW3_202202590_박종현.java
@@ -6,11 +6,13 @@ public class HW3_202202590_박종현 {
         int y = 45612;
         int result = gcd(x, y);
         System.out.printf("%d와 %d의 최대공약수 : "+result+"\n", x, y);
+        System.out.println(pow(2,14));
+        System.out.println(pow(3,8));
     }
 
     public long pow(int n, int x) {
         if (x == 0) { return 1; }
-
+        if (x == 1) {return n;}
         if (x%2 == 0){
             return pow(n*n, x/2);
         } else if (x%2 == 1){
